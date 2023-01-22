ba1baa9209dec276875aa91762fbe8d12fe0768e
vishnupriyan <priyanvishnu15@gmail.com>
2023-01-22 11:58:29 +0530

    added env folders

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment:{API_KEY: string, CX_KEY: string, SERVER_URL: string, production: boolean} = {
+    API_KEY: "AIzaSyBV1nRojt9SV5cZkYqeyboXkFcj_x6IY24",
+    production: true,
+    CX_KEY: '1407f173ab6ca48ec',
+    SERVER_URL: 'https://www.googleapis.com/customsearch/v1'
+};
